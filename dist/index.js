517cd141e1186beca2a978e16eddddb24fd460c2
SaffronGamer <saffrongamer167@gmail.com>
2024-12-13 21:34:24 -0700

    added styling for the repos div and the links dynamically populate upon load

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,19 @@
 import { get_repos } from "./service.js";
-console.log(await get_repos());
+let reposDivNode = document.getElementById('repo_placeholder');
+async function render_repos() {
+    let repos_list = await get_repos();
+    let repos = document.createElement('ul');
+    repos.className = 'flex-col';
+    repos_list.map((m) => {
+        let ulNode = document.createElement('li');
+        let repoLink = document.createElement('a');
+        repoLink.textContent = m.name;
+        repoLink.href = m.html_url;
+        repoLink.className = 'my-2 hover:text-green-400';
+        ulNode.appendChild(repoLink);
+        repos.appendChild(ulNode);
+    });
+    reposDivNode.replaceChildren(repos);
+}
+render_repos();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
 import { get_repos } from "./service.js";
 
-console.log(await get_repos())
+let reposDivNode: HTMLElement = document.getElementById('repo_placeholder')
+
+async function render_repos() {
+    let repos_list = await get_repos()
+    let repos: HTMLUListElement = document.createElement('ul')
+    repos.className = 'flex-col'
+
+    repos_list.map((m) => {
+        let ulNode: HTMLLIElement = document.createElement('li')
+        let repoLink: HTMLAnchorElement = document.createElement('a')
+        repoLink.textContent = m.name
+        repoLink.href = m.html_url
+        repoLink.className = 'my-2 hover:text-green-400'
+        ulNode.appendChild(repoLink)
+        repos.appendChild(ulNode)
+    });
+
+    reposDivNode.replaceChildren(repos)
+}
+
+render_repos()
