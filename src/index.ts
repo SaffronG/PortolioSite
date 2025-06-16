cf7155ab1c14a74697ed0eeab6e9cee30973d573
SaffronG <saffrongamer167@gmail.com>
2025-06-16 17:46:44 -0600

    fixed ts rendering the projects wrong

--- src/index.ts
+++ src/index.ts
@@ -20,28 +20,28 @@ async function render_repos() {
         visible = visible ? false : true
         if (visible) {
             mainDiv.className = "flex content-start divide-x-2 light:divide-gray-700 dark:divide-slate-600"
-            viewMoreButton.className = 'text-2xl border-solid m-2 justify-end m-auto hover:text-green-400 cursor-pointer'
+            viewMoreButton.className = 'text-2xl m-2 justify-end m-auto hover:text-green-400 cursor-pointer'
             viewMoreButton.textContent = '<<'
             leftBar.className = "transition-all w-auto"
             repos.className = "flex-col m-2"
-            projectHeader.className = 'text-2xl border-solid m-2 block'
+            projectHeader.className = 'text-2xl m-2 block'
         }
         else {
             mainDiv.className = "flex content-start"
-            viewMoreButton.className = 'text-2xl border-solid border-green-600 m-0 justify-end hover:text-green-400 cursor-pointer'
+            viewMoreButton.className = 'text-2xl border-green-600 m-0 justify-end hover:text-green-400 cursor-pointer'
             viewMoreButton.textContent = ">>"
             leftBar.className = "transition-all"
             repos.className = "hidden"
-            projectHeader.className = 'text-2xl border-solid m-2 hidden'
+            projectHeader.className = 'text-2xl m-2 hidden'
         }
     })
 
     repos_list.map((m) => {
         let ulNode: HTMLLIElement = document.createElement('li')
         let linkDiv: HTMLDivElement = document.createElement('div')
-        linkDiv.className = 'flex hover:text-green-400 hover:fill-green-400 content-start underline'
+        linkDiv.className = 'flex hover:text-green-400 hover:fill-green-400 content-start underline m-3 w-xs mr-5'
         let svgNode = document.createElement('div')
-        svgNode.innerHTML = '<svg class="fill-green-600 size-4 m-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M9.4 86.6C-3.1 74.1-3.1 53.9 9.4 41.4s32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 9.4 86.6zM256 416l288 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-288 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg>'
+        svgNode.innerHTML = '<svg class="fill-green-600 size-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M9.4 86.6C-3.1 74.1-3.1 53.9 9.4 41.4s32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L178.7 256 9.4 86.6zM256 416l288 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-288 0c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg>'
         let repoLink: HTMLAnchorElement = document.createElement('a')
         repoLink.innerHTML = m.name + ".exe"
         repoLink.href = m.html_url
